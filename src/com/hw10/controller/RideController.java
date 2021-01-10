51eae899f9b675bb59532bdd7f6698ca5a14f1fb
jinseon <fantasi89@gmail.com>
2021-01-10 13:44:20 +0900

    2021 0110 commit; 예외처리 어떻게하란거지?

--- src/com/hw10/controller/RideController.java
+++ src/com/hw10/controller/RideController.java
@@ -12,17 +12,19 @@ public class RideController {
 		
 		// 4명의 탑승 대기자를 생성자를 통한 GUest객체 배열 초기화(Guests[] gs)
 		// RollerCoaster클래스의 허용인원 상수값 크기의 또 다른 객체 배열 생성
-		// (guests[] onBooard)
+		// (guests[] onBooard)		
+
 		try {
 			// for문과 if문을 통해 RollerCoaster클래스의 허용키 상수값 크기
 			// 이상인 사람을 Guests객체 배열 onBoard에 담는다
-		}catch(){// 배열의 크기를 넘어가면 발생되는 예외클래스 참조형 변수 e를 받는 매개변수 자리
 			
-		// 문제가 바생한 해당 배열의 인덱스 번호가 몇번에서 발새ㅐㅇ하는지
-				// Throwable클래스의 getMessage()메소드를 통해 출력
+		} catch (Exception e) {// 배열의 크기를 넘어가면 발생되는 예외클래스 참조형 변수 e를 받는 매개변수 자리
+			// 문제가 발생한 해당 배열의 인덱스 번호가 몇번에서 발생하는지
+			// Throwable클래스의 getMessage()메소드를 통해 출력
+
 		}finally {
 			
 		}
-
+	}
 	
 }
