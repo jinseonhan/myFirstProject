bc1de72a39610b26a6aa3475aeb4d33bdfa33421
jinseonhan <fantasi89@gmail.com>
2021-01-12 09:37:38 +0900

    2021 0112 commit java 입출력 연습문제 양식 commit

---
+++ src/com/hw11/controller/FileController.java
@@ -0,0 +1,24 @@
+package com.hw11.controller;
+
+import java.util.Scanner;
+
+public class FileController {
+	Scanner sc= new Scanner(System.in);
+	
+	public FileController() {}
+	
+	public void fileSave() {
+		// 어떤 파일에 어떤 내용을 저장할지 입력 받아 파일 출력을 실행하는 메소드
+	}
+	
+	public void fileOpen() {
+		// 원하는 파일을 파일 입력 후 콘솔 화면에 출력하는 메소드
+	}
+	
+	public void fileEdit() {
+		// FileController 클래스 객체 생성 후 각 메소드를 메뉴 선택에 따라 실행
+	}
+	
+	
+	
+}
---
+++ src/com/hw11/run/NoteMenu.java
@@ -0,0 +1,7 @@
+package com.hw11.run;
+
+public class NoteMenu {
+	public static void main(String[] args) {
+		
+	}
+}
