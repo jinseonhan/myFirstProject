4c491450f2b2096d756371a8ed54b6e627cc2fdd
jinseonhan <fantasi89@gmail.com>
2021-01-13 09:44:40 +0900

    2021 0113 commit 파일/ String 입출력 예제 완성

--- src/com/hw11/controller/FileController.java
+++ src/com/hw11/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.hw11.controller;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileController {
@@ -9,14 +11,61 @@ public class FileController {
 	
 	public void fileSave() {
 		// 어떤 파일에 어떤 내용을 저장할지 입력 받아 파일 출력을 실행하는 메소드
+		StringBuilder sb = new StringBuilder();
+		// "파일에 저장할 내용을 입력하시오("exit"을 입력하면 내용 입력 끝);
+		// 값을 scannner로 입력 받아 StringBuilder에 저장하고, "exit"을 입력 했을 시 빠져나가는 while문 생성
+		
+		// "저장하시겠습니까(Y/N)"
+		// 입력 받은 값이 대문자이든 소문자이든 상ㅇ관없이 "y"이면,
+		// 저장할 파일명을 입력받음
+		try {
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			// 열었던 스트림 close
+		}
+		// 입력 받은 값이 Y가 아닌 어떤 값이든 "다시 메뉴로 돌아갑니다." 출력과 메소드
+		// 종료
 	}
 	
 	public void fileOpen() {
 		// 원하는 파일을 파일 입력 후 콘솔 화면에 출력하는 메소드
+		
+		// "열기 할 파일명 : "
+		// 파일명을 sCANNER로 입력받아 , bUFFEREDrEADER와 FileReader스트림 사용
+		try {
+			// try구문 안에서 readLine()메소드를 통해 한줄씩 콘솔로 입력받은 값 출력
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			// 열었던 슽트림 close
+		}
 	}
 	
 	public void fileEdit() {
 		// FileController 클래스 객체 생성 후 각 메소드를 메뉴 선택에 따라 실행
+		
+		// "수정할 파일명 : "
+		// 파일명을 입력받아 BufferedReader와 bufferedWriter, FileReader와 FileWriter
+		// 스트림 사용
+		
+		try {
+			// 반복문을 통해 해당 파일의 내용을 readLine()메소드를 통해 콘솔에 출력
+			// "파일에 추가할 내용을 입력하시오 : "
+			// 사용자가 "exit"를 입력하기 전까지 내용을 StringBuilder에 담는다.
+			
+			// "변경된 내용을 파일에 추가하시겠습니까?(y/n)"
+			// 입력받은 값이 대문자이든 소문자이든 상관없이 "y"이면,
+			// "입력받은 파일명. txt파일의의 내용이 변경되었습니다." 출력과 저장
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			// 열었던 스트림 close
+		}
+		
 	}
 	
 	
--- src/com/hw11/run/NoteMenu.java
+++ src/com/hw11/run/NoteMenu.java
@@ -1,7 +1,26 @@
 package com.hw11.run;
 
+import java.util.Scanner;
+
+import com.hw11.controller.FileController;
+
 public class NoteMenu {
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		FileController fc = new FileController();
+		
+		// 반복문을 통한 메뉴 실행
+		// ****** MyNote ******
+		System.out.println("1. 노트 새로 만들기");
+		System.out.println("2. 노트 열기");
+		System.out.println("3. 노트 열어서 수정하기");
+		System.out.println("4. 끝내기");
+		System.out.println("번호를 입력하세요 : ");
+		// 반복문 내에서 입력받은 번호에 따라서 FileController의 메소드를 실행
+		
+		// 4번의 끝내기는 "프로그램을 종료합니다." 라는 출력과 함께 프로그램이 종료되게 함
+		// 1~4번을 제외한 값을 입력 받으면 " 잘목입력하셨습니다."출력하고 메뉴 반복
+		
 		
 	}
 }
