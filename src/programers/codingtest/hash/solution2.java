f614a24d10de3f27e62d9cfc456d2455a9a247a0
jinseonhan <fantasi89@gmail.com>
2021-01-22 09:38:02 +0900

    2021 0122 commit 전화번호목록(startWith 함수)

--- src/programers/codingtest/hash/solution2.java
+++ src/programers/codingtest/hash/solution2.java
@@ -1,35 +1,27 @@
 package programers.codingtest.hash;
 
 class Solution2 {
-    
+	public static void main(String[] args){
+		
+		new Solution();
+
+	}  
+	
 	public boolean solution() {
 		String[] phone_book = {"119", "97674223", "1195524421"}; 
-        boolean answer = true;
-        int temp=0;
-        // 가장 짧은 자릿수 구하기
-        for(int i=0;i<phone_book.length;i++) {
-        	if(phone_book[i].length()<phone_book[temp].length()) {
-        		temp=i;
-        	}
-        }
-        int temp2=0;
-        for(int i=0;i<temp;i++) {
-        	if(i!=temp) {
-        		for(int j=0;j<phone_book[temp].length();j++) {
-        			if(phone_book[temp].charAt(j)==phone_book[i].charAt(j)) {
-        				temp2++;
-        			}
-        		}
-        		if(temp2==temp) {
-    				return answer=false;
-    			}else {
-    				temp2=0;
-    			}
-        	}
-        }
-        
-        return answer;
-    }
-    
-    
+		boolean answer = true;
+		// 문자열.startsWith(비교 문자열) : prefix값으로 시작하는지 확인하고 false/true로 반환
+		for(int i=0; i<phone_book.length;i++) {
+			for(int j=i+1; j<phone_book.length;j++) {
+				if(phone_book[i].startsWith(phone_book[j]))
+					return false;
+				if(phone_book[j].startsWith(phone_book[i]))
+					return false;
+			}
+		}
+
+		return answer;
+	}
+
+
 }
