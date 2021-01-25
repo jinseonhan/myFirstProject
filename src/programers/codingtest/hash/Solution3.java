4d13c250668cfa5fc9d70841020a7454329473dd
jinseonhan <fantasi89@gmail.com>
2021-01-25 09:31:01 +0900

    2021 0125 commit; java 프로그래머스 해시 답안 (아직 이해 잘 안됨,..)

--- src/programers/codingtest/hash/Solution3.java
+++ src/programers/codingtest/hash/Solution3.java
@@ -1,28 +1,26 @@
 package programers.codingtest.hash;
 
 import java.util.HashMap;
+import java.util.Set;
 
 class Solution3 {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1; // 1. 곱셈을 위해 1로 선언
         
-        
-        // 1.key를 확인하며 없으면, 키를 추가한다.
-        // 2.
-        
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
-       
-        for(String s:clothes) {
-        	if(!hm.containsKey(s[1])) {
-        		hm.put(s[1],1);
-        	}else {
-        		hm.put(s[1], hm.get(s[1])+1);
-        	}
+        HashMap<String,Integer> clothesMap = new HashMap<>();
+        // 2. map 구하기   
+        for(int i=0; i<clothes.length; i++) {
+        	// 3. 의상 종류, 갯수
+        	clothesMap.put(clothes[i][1], clothesMap.getOrDefault(clothes[i][1], 0)+1);
         }
         
+        // 4. 조합
+        Set<String> keySet =clothesMap.keySet();
         
+        for(String key:keySet) {
+        	answer*=clothesMap.get(key)+1;
+        }
         
-        
-        return answer;
+        return answer-1; // answer에서 아무것도 안입었을 경우의 수를 제거
     }
 }
