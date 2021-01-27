d61128447005127c7d4f0d8b9097157ecff2a1e1
jinseonhan <fantasi89@gmail.com>
2021-01-27 09:36:17 +0900

    2021 0127 commit; 답을 보았는데 무슨 뜻인지 이해 안감 혼자 생각해보는게 나을듯

--- src/programers/codingtest/hash/Solution4.java
+++ src/programers/codingtest/hash/Solution4.java
@@ -6,25 +6,23 @@ public class Solution4 {
 	 public int[] solution(String[] genres, int[] plays) {
 	        int[] answer = {};
 	        int[] out =new int[4];
-	        int temp=0;
-	        int sum=0;
-	        String str = "";
-	        int a=0;
-	        // 1.장르의 갯수를 알아야함 key 에 넣고 없으면 저장
-	        HashMap<str, a> map = new Hashmap();
-	        // 2.장르별 가장 많이 재생 된 노래를 2개씩 저장
-	        
+
+	        HashMap<String, Integer> map = new HashMap<>(); // 장르, 횟수 저장하고
+	        	        
+	        // 1.해시 맵으로 옮기기
 	        for(int i=0;i<plays.length;i++) {
-	        	if(plays[i]>temp) {
-	        		
-	        		temp=plays[i]; // 가장 큰 값 구하기
-	        		str=genres[i];
-	        	}
+	        	map.put(genres[i], plays[i]);
+	        	
+//	        	if(plays[i]>temp) {
+//	        		
+//	        		temp=plays[i]; // 가장 큰 값 구하기
+//	        		str=genres[i];
+//	        	}
 	        }
-	        
-	        for(int i=0;i<out.length;i++) {
-	        }
-	        
+
+	        // 1.속한 노래가 많이 재생된 장르를 먼저 수록
+	        // 2.장르내에서 많이 재생된 노래를 먼저수록
+	        // 3.장르 내에서 재생 횟수가 같은 노래중에서는 고유번호가 낮은 노래를 먼저 수록
 	        
 	        return answer;
 	    }
