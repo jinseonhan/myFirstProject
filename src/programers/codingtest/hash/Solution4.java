ebb854cdfc5ab1b89b3fa02df677b300711b15ad
jinseonhan <fantasi89@gmail.com>
2021-01-28 09:53:29 +0900

    2021 0128 commit; 프로그래머스 스택문제 왜이래?

--- src/programers/codingtest/hash/Solution4.java
+++ src/programers/codingtest/hash/Solution4.java
@@ -1,13 +1,26 @@
 package programers.codingtest.hash;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
+// 문자열 배열이 주어지고 중복되는 문자열을 걸러야 로직을 작성해야할 경우 해시를 주로 사용한다.(특히 그 문자열을 다시 반환하지 않을 경우 사용)
 public class Solution4 {
+	
+	public static void main(String[] args) {
+
+		String[] genres = { "classic", "pop", "classic", "classic", "pop", "dance", "k-pop",
+				"k-pop", "k-pop" };
+		int[] plays = { 500, 600, 150, 800, 2500, 1000, 2000, 3000, 500 };
+
+		
+	}
+	
 	 public int[] solution(String[] genres, int[] plays) {
 	        int[] answer = {};
 	        int[] out =new int[4];
 
-	        HashMap<String, Integer> map = new HashMap<>(); // 장르, 횟수 저장하고
+	        Map<String, Integer> map = new HashMap<>(); // 장르, 횟수 저장하고
 	        	        
 	        // 1.해시 맵으로 옮기기
 	        for(int i=0;i<plays.length;i++) {
---
+++ src/programers/codingtest/stackQ/Solution1.java
@@ -0,0 +1,10 @@
+package programers.codingtest.stackQ;
+
+public class Solution1 {
+	 public int[] solution(int[] prices) {
+	        int[] answer = {};
+	        // 1. 총 증가한 i값을 담을 변수가 필요
+	        
+	        return answer;
+	    }
+}
