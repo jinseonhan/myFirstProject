0287529f35f51ca4df48dd7f0778abbab46f82b5
jinseonhan <fantasi89@gmail.com>
2021-01-28 09:54:20 +0900

    2021 0128 commit
    스택 왜 쓰는지 모르겠음

--- src/programers/codingtest/stackQ/Solution1.java
+++ src/programers/codingtest/stackQ/Solution1.java
@@ -4,7 +4,7 @@ public class Solution1 {
 	 public int[] solution(int[] prices) {
 	        int[] answer = {};
 	        // 1. 총 증가한 i값을 담을 변수가 필요
-	        
+	        // ???
 	        return answer;
 	    }
 }
