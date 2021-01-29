add99ef0f2371245da0cffbb1bbe255c37d8da6a
jinseonhan <fantasi89@gmail.com>
2021-01-29 09:26:39 +0900

    2021 0129 commit 내일 해결하기

--- src/programers/codingtest/stackQ/Solution1.java
+++ src/programers/codingtest/stackQ/Solution1.java
@@ -5,6 +5,8 @@ public class Solution1 {
 	        int[] answer = {};
 	        // 1. 총 증가한 i값을 담을 변수가 필요
 	        // ???
+	        	// 스택 어쩌라는거야?
+	        // 4다음 숫자는 뭐임?
 	        return answer;
 	    }
 }
