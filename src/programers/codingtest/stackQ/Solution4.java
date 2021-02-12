07807c544378a6e63eddb1731255954d98bbca96
jinseonhan <fantasi89@gmail.com>
2021-02-13 08:46:20 +0900

    2021 0213 commit; 여기서는 되는데 왜 프로그래머스에서는 안됨? 어이가 없네?

--- src/programers/codingtest/stackQ/Solution4.java
+++ src/programers/codingtest/stackQ/Solution4.java
@@ -1,38 +1,74 @@
 package programers.codingtest.stackQ;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution4 {
-	
-	    public int solution() {
-	        int answer = 0;
-	        int[] priorties = {1,1,1,1,1,4,1,2,4,2};
-	        int location = 4;
-	        int topIndex=0; // 그 인덱스를 담는다.
-	        int topScore =0; // 가장 높은값을 찾는다
-	        // 1. 우선순위가 높은 인덱스를 찾는다.
-	        // 2-1. location이 우선순위보다 값이 크다면 [자기자신-우선순위+1] 
-	        // 2-2. location이 우선순위보다 값이 작다면 [총길이-우선순위-자기자신+1]
-	        // 2-3. location 자신이 우선순위가 가장 높다면 [1]로 반환
-	        
-	        // 먼저 우선순위가 가장 높은 index를 찾자
-	        for(int i=0;i<priorties.length;i++) {
-	        	if(topScore<priorties[i]) {
-	        		topScore=priorties[i];
-	        		topIndex=i; // 높은 인덱스 값을 담는다.
-	        	}
-	        }
-	        System.out.println("topIndex의 값 : "+topIndex);
-	        if(topIndex>location) {
-	        	System.out.println("topIndex>location");
-	        	answer=priorties.length-topIndex-location+1;
-	        }else if(topIndex==location) {
-	        	System.out.println("topIndex==location");
-	        	answer=location-topIndex+1;
-	        }else {
-	        	System.out.println("topIndex<location");
-	        	answer=1;
-	        }  
-	        return answer;
-	    }
+	 
+	class Original{
+		int prior; // 우선순위
+		int lo; // 본래의 location
+		public int getPrior() {
+			return prior;
+		}
+		public void setPrior(int prior) {
+			this.prior = prior;
+		}
+		public int getLo() {
+			return lo;
+		}
+		public void setLo(int lo) {
+			this.lo = lo;
+		}
+		public Original(int prior, int lo) {
+			this.prior = prior;
+			this.lo = lo;
+		}
+		
+	}
+    public int solution() {
+        int answer = 0;
+        int[] priorities = {1,1,9,1,1,1};
+        int location = 4;
+        int topScore =0; // 가장 높은값을 찾는다        
+        Queue<Original> q = new LinkedList<>();
+        
+        // 반복문으로 key value와 같은 형식으로 큐에 담는다.
+        for(int i=0;i<priorities.length;i++) {
+        	q.offer(new Original(priorities[i], i));
+        }
+        // 무한으로 돌리고 location =4일때 반환 시킨다.
+        int tempNum=0;
+        while(true) {
+        	tempNum = q.size();
+        	
+        	// 가장 높은 숫자를 찾는다.
+        	for(int j=0;j<tempNum;j++) {
+        		if(q.peek().getPrior()>topScore) {
+        			topScore=q.peek().getPrior(); 
+        		}
+        		q.add(q.peek());
+        		q.remove();
+        	}
+        	// 가장 높은 숫자들을 반복해서 뺀다.
+        	for(int h=0;h<tempNum;h++) {
+        		if(q.peek().getPrior()==topScore) {
+        			answer++;
+        			if(q.peek().getLo()==location) {
+        				return answer;
+        			}
+        			q.remove();
+        		}else {
+    				q.add(q.peek());
+    				q.remove();
+    			}
+        	}
+        	topScore=0;
+        }
+    }
+    
+	   
+	    
 	public static void main(String[] args) {
 		System.out.println(new Solution4().solution());
 	}
