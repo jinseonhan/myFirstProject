26574c173ead583930178ab09f8b536cca1f9c04
jinseonhan <fantasi89@gmail.com>
2021-02-14 10:10:27 +0900

    2021 0214 commit; 우선순위 큐 적용

--- src/programers/codingtest/heap/Solution.java
+++ src/programers/codingtest/heap/Solution.java
@@ -1,6 +1,9 @@
 package programers.codingtest.heap;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 // Heap 이란?
 // 1.완전 이진 트리의 일종으로 우선순위 큐를 위하여 만들어진 자료구조
@@ -27,35 +30,74 @@ import java.util.ArrayList;
 // 특정 위치의 노드 번호는 새로운 노드가 추가되어도 변하지 않는다.
 
 public class Solution {
-	public int solution() {
+	public int solution1() {
 		int[] scoville = {1,2,3,8,10,12};
 		int K = 7;
 		int answer = 0; // 음식을 섞은 횟수(반환할 값)
 		
 		ArrayList<Integer> mix = new ArrayList<Integer>();
 		
+		
 		for(int i =0;i<scoville.length;i++) {
 			if(scoville[i]<K) {
 				mix.add(scoville[i]);
 			}
 		}
-
+		
+		// 
 		while(mix.get(0)<K&&mix.size()>=2) {
 			// 반복 횟수
 			answer++;
+			mix.sort(null); // 오름차순
 			
 			mix.add(mix.get(0)+(mix.get(1)*2));
 			mix.remove(0);
 			mix.remove(0);
 
-			mix.sort(null); // 오름차순
 		}
 		
 		
 		return answer;
 	}
 	
+	public int solution2() {
+		int[] scoville = {1,2,3,8,10,12};
+		int K = 7;
+		int answer = 0; // 음식을 섞은 횟수(반환할 값)
+		
+		PriorityQueue<Integer> heap = new PriorityQueue<>(); // 우선순위가 낮은 순서의 큐 생성
+		//PriorityQueue<Integer> heapEx = new PriorityQueue<>(Collections.reverseOrder()); // 우선순위가 높은 순서의 큐 생성
+		
+		// 객체에 기존 배열의 값을 담는다.
+		for(int i : scoville) {
+			heap.offer(i);
+		}
+		
+		// 큐의 처음 값이 K보다 작을 경우 반복한다.
+		while(heap.peek()<K) {
+			// 음식을 섞었는대도 남은 size가 1일 경우에는 더이상 K보다 커질 수 없다.
+			if(heap.size()==1) {
+				return -1;
+			}
+			
+			// 우선순위가 낮은 값들 2개를 변수에 담아
+			int a = heap.poll();
+			int b = heap.poll();
+			
+			// 음식을 섞는다.
+			int result = a+(b*2);
+			
+			// 섞은 음식을 다시 큐에 담는다.
+			heap.offer(result);
+			
+			// 섞은 횟수를 증가시킨다.
+			answer++;
+		}
+		return answer;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(new Solution().solution());
+//		System.out.println(new Solution().solution1());
+		System.out.println(new Solution().solution2());
 	}
 }
