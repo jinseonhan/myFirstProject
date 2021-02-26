13f3aca2ba71777fed7856f036f7843dfa4d82a1
jinseonhan <fantasi89@gmail.com>
2021-02-26 09:21:37 +0900

    2021 0226 commit 파견 나가기전에 프로젝트 맡은 부분 완료하자

--- src/programers/codingtest/heap/ExSolution.java
+++ src/programers/codingtest/heap/ExSolution.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
-// 주말에 파악합시다!
+// 주말에 파악합시다! 주말에 노트북에 세팅하고 TEST
 public class ExSolution {
 	 class Job {
 	        int requestTime;
