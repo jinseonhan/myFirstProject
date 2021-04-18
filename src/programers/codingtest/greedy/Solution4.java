8d94a2d6bf7322ff0190c0f30a27101c9a1553df
jinseonhan <fantasi89@gmail.com>
2021-04-18 10:11:46 +0900

    2021 0418 commit

--- src/programers/codingtest/greedy/Solution4.java
+++ src/programers/codingtest/greedy/Solution4.java
@@ -10,8 +10,7 @@ public class Solution4 {
 	        // 1. 내림차순으로 만든다.
 	        Arrays.sort(people);
 
-	        // 2. 남아있는 가장 작은 값을 가장 큰수부터 비교하여 limit보다 작거나 같은지 비교한 후
-	        // 조건을 만족한다면 카운트하고, 최대값보다 더 큰값으로 치환하여 남은 숫자를 셀 때 카운트 하지 않도록 한다.
+	        // 2. 혼자만 탑승해야하는 사람들을 센다
 	        for(int i=0;i<=leng;i++,answer++) {
 	        	  while(leng > i && people[i] + people[leng--] > limit) 
 	                  answer++;
---
+++ src/programers/codingtest/greedy/Solution5.java
@@ -0,0 +1,8 @@
+package programers.codingtest.greedy;
+
+public class Solution5 {
+	public int solution(int n, int[][] costs) {
+        int answer = 0;
+        return answer;
+    }
+}
