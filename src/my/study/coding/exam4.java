0fe47c28f89fcfcb41d89c9cef52ba3bcf00f950
jinseonhan <fantasi89@gmail.com>
2021-06-21 22:40:25 +0900

    [124나라] 문제 완료

--- src/my/study/coding/exam4.java
+++ src/my/study/coding/exam4.java
@@ -6,34 +6,29 @@ package my.study.coding;
 // ex 1 -> 1 , 2->2, 3-> 4 , 4->11 , 5 ->12 , 6->14 , 7->21 , 8->22 , 9 -> 24 , 10 ->41
 public class exam4 {
 	 public String solution(int n) {
-	        String answer = "";
-	        int quo =n;
-	        String temp="";
-	        
-	        
-	        // 3보다 몫이 작을떄까지 나눈다.
-	        while(quo>=3) { // 나머지가 0일떄까지 반복   //5, 1
-	        	temp+=Integer.toString(quo%3); // 나머지를 담는다. //  temp = 2
-	        	quo=quo/3; // 몫 저장 //1
-	        }
-	        // 나머지 0을 먼저 담고
-	        // 
-	        temp+=Integer.toString(quo); // temp = 21
-	        char change[] = temp.toCharArray(); // change = [2,1]
+        String answer = "";
+        int quo =0;
+        
+        
+        // 3보다 몫이 작을떄까지 나눈다.
+        while(n>0) { // 나머지가 0일떄까지 반복   //5, 1
+        	quo=n%3; // 나머지를 담는다. //  temp = 2
+        	n=n/3; // 몫 저장 //1
+        
+        	if(quo==0) {
+        		n-=1;
+        		quo=4;
+        	}
 
-	        for(int i=change.length-1;i==0;i++) {  // 
-	        	switch(change[i]) {
-	        		case '0': answer+="1";break;
-	        		case '1': answer+="2";break;
-	        		case '2': answer+="4";break;
-	        	}
-	        }
-	        System.out.println(answer);
-	        return answer;
+        	answer = quo+ answer;
+        }
+        
+        
+        return answer;
     }
 	 
 	 
 	public static void main(String[] args){
-		System.out.println(new exam4().solution(1));
+		System.out.println(new exam4().solution(10));
 	}
 }
