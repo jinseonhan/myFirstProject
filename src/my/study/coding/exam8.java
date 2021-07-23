8900703ea69bd156d88de7c51a2133f7bd52514d
jinseonhan <fantasi89@gmail.com>
2021-07-23 21:26:28 +0900

    reset

--- src/my/study/coding/exam8.java
+++ src/my/study/coding/exam8.java
@@ -1,36 +1,103 @@
 package my.study.coding;
-// 숫자의 표현(연속된 자연수 합의 갯수)
-//Finn은 요즘 수학공부에 빠져 있습니다. 수학 공부를 하던 Finn은 자연수 n을 연속한 자연수들로 표현 하는 방법이 여러개라는 사실을 알게 되었습니다. 예를들어 15는 다음과 같이 4가지로 표현 할 수 있습니다.
-//
-//1 + 2 + 3 + 4 + 5 = 15
-//4 + 5 + 6 = 15
-//7 + 8 = 15
-//15 = 15
-//자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
-//
-//제한사항
-//n은 10,000 이하의 자연수 입니다.
-public class exam8 {
-	public int solution(int n) {
-        int answer = 0;
-        
-        int total = 1;
-        int count = 1; // 연속된 숫자의 갯수 카운트
-        
-        do {
-        	System.out.println("count : "+ count+", total : "+total);
-	     	if(((n-total)%count)==0) {
-	    		answer++;
-	    	}
-	     	count++;
-	     	total=total+count;
-   
-        }while(total<=n);
 
-        return answer;
-    }
+public class exam8 {
 	
+	public int[] solution(String[][] str) {
+		int[] answer = new int[str.length];
+		 
+			 boolean chk =true;
+			 int count=0; // 붙어있는 사람이 있는지 확인
+			 
+			 for(int i=0;i<str.length;i++) {
+				 for(int j=0;j<str[i].length;j++) {
+					 // p일때와, O일때 체크
+					 String part= str[i][j];
+					 
+					 for(int k=0;k<part.length();k++) {
+						 char idx=part.charAt(k);
+						 if(idx=='P') { // 파티션일 때 count가 2이상되면 안됨
+							// 빈공간 확인
+							try {
+								if(str[i][j+1].charAt(k)=='O') {
+										count++;
+								}
+								if(str[i][j-1].charAt(k)=='O') {
+										count++;
+								}
+								if(str[i][j].charAt(k+1)=='O') {
+										count++;
+								}
+								if(str[i][j].charAt(k-1)=='O') {
+										count++;
+								}
+								
+								if(count>1) {
+									chk=false;
+									count=0; //count값 초기화;
+									break;
+								}
+								count=0; //count값 초기화
+							} catch (Exception e) {
+								// TODO: handle exception
+							}
+						 }else if(idx=='O') { // 사람일 때 count가 1이상 되면 안됨
+							 try {
+									if(str[i][j+1].charAt(k)=='O') {
+											count++;
+									}
+									if(str[i][j-1].charAt(k)=='O') {
+											count++;
+									}
+									if(str[i][j].charAt(k+1)=='O') {
+											count++;
+									}
+									if(str[i][j].charAt(k-1)=='O') {
+											count++;
+									}
+									
+									if(count>0) {
+										chk=false;
+										count=0; //count값 초기화;
+										break;
+									}
+									count=0; //count값 초기화
+								} catch (Exception e) {
+									// TODO: handle exception
+								}	 
+						 }
+					 
+					 // boolean 값이 false일 경우 false를 반환하고 원래 메소드에서도 false를 반환하여 끝낸다.
+					 count=0; // count값 초기화;
+				 }
+				 
+					 if(chk==false) {
+						 break;
+					 }
+				 
+				 }
+				 
+				 if(chk==false) {
+					 answer[i]=0;
+				 }else {
+					 answer[i]=1;
+				 }
+				 
+				 chk=true; //chk초기화
+			 }
+			
+		 return answer;
+	 }
+
+
+	 
+	 
+	 
 	public static void main(String[] args) {
-		System.out.println(new exam8().solution(15));
+		String[][] str = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+		int [] answer =new exam8().solution(str);
+		for(int i=0;i<answer.length;i++) {
+			System.out.print(answer[i]+" ");
+		}
+		//	결과값 : [1, 0, 1, 1, 1] 
 	}
 }
