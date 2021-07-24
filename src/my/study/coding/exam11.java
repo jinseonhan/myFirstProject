6ecd5cc0286dd5068a7e86b304e20129105ab031
jinseonhan <fantasi89@gmail.com>
2021-07-24 13:01:34 +0900

    프로그래머스 N개의 최소공배수 구하는 중

--- src/my/study/coding/exam11.java
+++ src/my/study/coding/exam11.java
@@ -1,36 +1,38 @@
 package my.study.coding;
 
-import java.util.Arrays;
 
 // 최소 공배수 구하기
 public class exam11 {
 	 public int solution(int[] arr) {
-	     int answer=1;   
-		 int gcd=1; 
-	        
-	        
-        Arrays.sort(arr);
+	     int answer=arr[0];   
+		
+	     for(int i=1;i<arr.length;i++) {
+	    	 answer = LCM( answer, arr[i]);
+	     }
         
-        for(int i=0;i<arr.length;i++) {
-        	int temp=1;
-        	for(int j=1;j<=(arr[i]/2);j++) {
-        		if(arr[i]%j==0 && gcd%j==0) {
-        			temp=j;
-        		}
-        	}
-        	if(temp>gcd) {
-        		gcd=temp;
-        	}
-        }	        
-        // 최대 공약수를 구한다.
-        // 모든 수를 곱하고 최대공약수로 나눈다.
-        for(int i=0;i<arr.length;i++) {
-        	answer*=arr[i];
-        }
-        System.out.println("gcd : "+ gcd);
-        answer/=gcd;
-        
-        return answer;
+        return answer; // 모든 수의 최소 공배수
+	 }
+	 
+	 public int LCM(int a, int b) {
+		 
+		 return a*b/GCD(a,b); // 최소 공배수
+	 }
+	 
+	 public int GCD(int a, int b) {
+		 // a가 큰 수, b가 작은수로 만들기
+		 if(a<b) {
+			 int temp = b;
+			 b=a;
+			 a=temp;
+		 }
+		 
+		 while(b!=0) {
+			 int r= a&b;
+			 a=b;
+			 b=r;
+		 }
+		 return a; // 최대 공약수
+		 
 	 }
 	 
 	 public static void main(String[] args) {
